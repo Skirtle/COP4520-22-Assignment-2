8bda9dddd2044e1ec4ff6b7d924c683c887a4fb4
Dalton <daltonkajander@knights.ucf.edu>
2022-03-03 16:00:09 -0500

    q

--- Homework 2/HW2-2.cpp
+++ Homework 2/HW2-2.cpp
@@ -1,9 +1,5 @@
 /*
-N = number of guests
-Only guest 0 can replace cupcake
-Guest R = random guest (excluding guest 0)
-
-0.0 One guest at a time
+1.0 One guest at a time
 */
 
 // Includes
@@ -12,16 +8,73 @@ Guest R = random guest (excluding guest 0)
 #include<cstdlib>
 
 // Defines
-#define MIN_RAND_N 5
-#define MAX_RAND_N 15
-#define GUEST_COUNT 12
-
-
-// Globals
-
+#define MIN_RAND_N 0
+#define MAX_RAND_N 14
+#define DEF_GUEST_COUNT 12
 
 // Function prototypes
 int randomInt(int min, int max); // A random number between min and max, inclusive of both
+void guestThread(int tid) {
+
+}
+
+// Classes
+class Queue {
+public:
+	int size = 0;
+	int maxSize;
+
+	Queue(int length) {
+		arr = (int*)malloc(length * sizeof(int));
+		maxSize = length;
+		if (arr != NULL) {
+			for (int i = 0; i < maxSize; i++) {
+				arr[i] = -1;
+			}
+		}
+	}
+	
+	~Queue() {
+		free(arr);
+	}
+
+	int dequeue() {
+		if (size <= 0) return -1;
+		int front = arr[0];
+		for (int i = 0; i < maxSize - 1; i++) {
+			arr[i] = arr[i + 1];
+		}
+		arr[maxSize - 1] = -1;
+		size--;
+		return front;
+	}
+
+	int peek() {
+		if (size <= 0) return -1;
+		return arr[0];
+	}
+
+	void enqueue(int val) {
+		if (size < maxSize) {
+			arr[size] = val;
+			size++;
+		}
+	}
+
+	void print() {
+		for (int i = 0; i < maxSize; i++) {
+			std::cout << arr[i] << " ";
+		}
+		std::cout << "\n";
+	}
+
+private:
+	int* arr;
+};
+
+// Globals
+bool roomOccupied = false;
+Queue queue = Queue(DEF_GUEST_COUNT);
 
 
 int main() {
@@ -32,6 +85,7 @@ int main() {
 	return 0;
 }
 
+// Function defines
 int randomInt(int min, int max) {
 	return rand() % (max - min + 1) + min;
 }
