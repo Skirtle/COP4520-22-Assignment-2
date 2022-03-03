2f47249be5349e5609a2e368c576930221c29d49
Dalton <daltonkajander@knights.ucf.edu>
2022-03-02 22:35:53 -0500

    Update HW2-1.cpp

--- HW2/HW2/HW2-1.cpp
+++ HW2/HW2/HW2-1.cpp
@@ -1,71 +1,66 @@
-// Includes
-#include <iostream>
-#include <thread>
-
-// Defines
-#define MIN_RAND_N 5
-#define MAX_RAND_N 15
-
-
-// Globals
-int guestCount; // Amount of guests
-int currentGuestID = -1; // The guest currently in the maze
-int counter = 1; // includes the Counting Guest
-bool cupcakeIsThere = true; // Cupcake exists at start
-bool waiting = true; // Counting not done yet
-
-/* N = number of guests
+/* 
+N = number of guests
 Only guest 0 can replace cupcake
 Guest R = random guest (excluding guest 0)
 
+0.0 One chosen guest at a time
 1.0 If guest 0 sees no cupcake, add 1 to counter and replace cupcake
 2.0 If guest 0 sees cupcake, do nothing
 3.0 If guest R sees no cupcake, do nothing
 4.0 If guest R sees cupcake:
 	4.1 If already eaten one, do nothing
-	4.2 If not already eaten one, eat cupcake */
+	4.2 If not already eaten one, eat cupcake
+*/
+
+// Includes
+#include <iostream>
+#include <thread>
+#include<cstdlib>
+
+// Defines
+#define MIN_RAND_N 5
+#define MAX_RAND_N 15
+#define GUEST_COUNT 15
+
+
+// Globals
+int currentGuestID = -1; // The guest currently in the maze
+int counter = 1; // includes the Counting Guest
+bool cupcakeIsThere = true; // Cupcake exists at start
+bool waiting = true; // Counting not done yet
 
 
 // Function prototypes
 int randomInt(int min, int max); // A random number between min and max, inclusive of both
 void guestThread(int id);
 
+
 int main() {
-	// Choosing the amount of guests
-	std::cout << "How many guests? Enter 0 or less for a random number between 5 and 15\n";
-	std::cin >> guestCount;
-	if (guestCount <= 0) {
-		guestCount = randomInt(MIN_RAND_N, MAX_RAND_N);
-	}
-	std::cout << "guestCount chosen to be " << guestCount << std::endl;
+	// Start
+	srand(time(0));
 
 	// Create threads
-	// std::thread* threads = (std::thread*) malloc(guestCount * sizeof(std::thread));
-
-	std::thread threads[15];
-	if (threads != NULL) { // In case not enough space for the threads to be allocated as an array
-		for (int i = 0; i < guestCount; i++) {
-			std::cout << "Creating thread " << i << std::endl;
-			threads[i] = std::thread(guestThread, i);
-		}
+	std::thread threads[GUEST_COUNT];
+	for (int i = 0; i < GUEST_COUNT; i++) {
+		threads[i] = std::thread(guestThread, i);
 	}
 
+
+	// Wait for guests do to everything
 	while (waiting) {
+		currentGuestID = randomInt(0, GUEST_COUNT - 1);
 		while (currentGuestID != -1) {
 			continue;
 		}
-		int x = randomInt(0, guestCount - 1);
-		// std::cout << "Random guest " << x << " is up!\n";
-		currentGuestID = x;
-		
 	}
-	std::cout << "Done!\n";
 
-	for (int i = 0; i < guestCount; i++) {
+
+	// Free threads when done
+	for (int i = 0; i < GUEST_COUNT; i++) {
 		threads[i].join();
 	}
 
-	// free(threads);
+	// Finish
 	std::cout << "Counted " << counter << " guests\n";
 	return 0;
 }
@@ -75,43 +70,47 @@ int randomInt(int min, int max) {
 }
 
 void guestThread(int tid) {
-	std::string msg = "Starting thread " + std::to_string(tid) + "\n";
-	// std::cout << msg;
-
 	bool eaten = false;
-
-	while (tid != currentGuestID && waiting) {
-		continue;
-	}
-
-	while (!waiting) {
-		continue;
-	}
-		
-	msg = "Thread " + std::to_string(tid) + " in the maze\n";
-	// std::cout << msg;
-
-	if (tid == 0 && !cupcakeIsThere) { // Rule 1.0
-		cupcakeIsThere = true;
-		counter++;
-		if (counter == guestCount) {
-			waiting = false;
-			return;
+	while (waiting) {
+		// Rule 0.0 One chosen guest at a time
+		if (tid != currentGuestID) {
+			continue;
 		}
-	} // Rule 2.0
 
-	else if (tid != 0) { // Not guest 0
-		if (cupcakeIsThere) { // Rule 4.0
-			if (!eaten) { // Rule 4.2
+		// Rule 1.0 If guest 0 sees no cupcake, add 1 to counter and replace cupcake
+		else if (tid == 0 && !cupcakeIsThere) {
+			counter++;
+			cupcakeIsThere = true;
+			if (counter == GUEST_COUNT) {
+				waiting = false;
+			}
+		}
+
+		// Rule 2.0 If guest 0 sees cupcake, do nothing
+		else if (tid == 0 && cupcakeIsThere) {
+			// do nothing
+		}
+
+		// Rule 3.0 If guest R sees no cupcake, do nothing
+		else if (tid != 0 && !cupcakeIsThere) {
+			// do nothing
+		}
+
+		// Rule 4.0 If guest R sees cupcake
+		else if (tid != 0 && cupcakeIsThere) {
+			// Rule 4.1 If already eaten one, do nothing
+			if (eaten) {
+				// do nothing
+			}
+
+			// Rule 4.2 If not already eaten one, eat cupcake
+			else if (!eaten) {
 				eaten = true;
 				cupcakeIsThere = false;
-			} // Rule 4.1
-		} // Rule 3.0
+			}
+		}
+
+		// Guest leaves
+		currentGuestID = -1;
 	}
-
-
-	currentGuestID = -1;
-
-	msg = "Thread " + std::to_string(tid) + " is done\n";
-	// std::cout << msg;
 }
